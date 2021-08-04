2cc9d719d87758adbf274f648900a1b579262bcd
Storozheva Svetlana <namelessjoel@yandex.ru>
2021-08-04 21:53:41 +0300

    __it__ added

--- main_1.py
+++ main_1.py
@@ -10,6 +10,9 @@ class Student:
     def add_courses(self, course_name):
         self.finished_courses.append(course_name) 
 
+    def add_courses_in_progress(self, course_name):
+        self.courses_in_progress.append(course_name)
+
     def rate_lecturer(self, lecturer, course, grade):
         if isinstance(lecturer, Lecturer) and course in self.courses_in_progress:
             if course in lecturer.lecturer_grades:
@@ -19,8 +22,23 @@ class Student:
         else:
             return 'Ошибка'  
 
+    def get_average_grade(self, grade):
+        self.grades = grade.values()
+        grade = []
+        for grades in self.grades:
+            for number in grades:
+                grades.append(number)
+        return round(sum(grade) / len(grade), 1)
+
+    def __lt__(self, other_student):
+        if not isinstance(other_student, Student):
+            print('Такого студента нет')
+            return
+        else:
+            compare = self.get_average_grade() < other_student.get_average_grade() 
+
     def __str__(self):
-        res =(f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за домашние задания: {self.grades}\nКурсы в процессе изучения: {self.courses_in_progress}\nЗавершенные курсы: {self.finished_courses}')
+        res =(f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за домашние задания: {self.get_average_grade}\nКурсы в процессе изучения: {self.courses_in_progress}\nЗавершенные курсы: {self.finished_courses}')
         return res
  
      
@@ -59,10 +77,10 @@ class Reviewer(Mentor):
         
 
 student_1 = Student('Emma', 'Stone', 'Gender')
-student_1.courses_in_progress += ['Python']
 
 reviewer_1 = Reviewer('Some', 'Buddy')
-reviewer_1.courses_attached += ['Python']
+student_1.add_courses_in_progress('Python')
+reviewer_1.courses_attached = ['Python']
 
 reviewer_1.rate_hw(student_1, 'Python', 10)
 
