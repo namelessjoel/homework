f837945cc31c49d6d56362d81ff266e781bea822
Storozheva Svetlana <namelessjoel@yandex.ru>
2021-08-06 22:36:25 +0300

    incorrect 4 task

--- main_1.py
+++ main_1.py
@@ -6,6 +6,8 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
+        self.courses_attached = []
+        self.lecturer_grades = {}
  
     def add_courses(self, course_name):
         self.finished_courses.append(course_name) 
@@ -14,7 +16,7 @@ class Student:
         self.courses_in_progress.append(course_name)
 
     def rate_lecturer(self, lecturer, course, grade):
-        if isinstance(lecturer, Lecturer) and course in self.courses_in_progress:
+        if isinstance(lecturer, Lecturer) and course in self.courses_in_progress and course in lecturer.courses_attached:
             if course in lecturer.lecturer_grades:
                 lecturer.lecturer_grades[course] += [grade]
             else:
@@ -22,10 +24,9 @@ class Student:
         else:
             return 'Ошибка'  
 
-    def get_average_grade(self, grade):
-        self.grades = grade.values()
+    def get_average_grade(self):
         grade = []
-        for grades in self.grades:
+        for grades in self.grades.values():
             for number in grades:
                 grades.append(number)
         return round(sum(grade) / len(grade), 1)
@@ -35,10 +36,10 @@ class Student:
             print('Такого студента нет')
             return
         else:
-            compare = self.get_average_grade() < other_student.get_average_grade() 
+            return self.get_average_grade() < other_student.get_average_grade() 
 
     def __str__(self):
-        res =(f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за домашние задания: {self.get_average_grade}\nКурсы в процессе изучения: {self.courses_in_progress}\nЗавершенные курсы: {self.finished_courses}')
+        res =(f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за домашние задания: {self.get_average_grade()}\nКурсы в процессе изучения: {self.courses_in_progress}\nЗавершенные курсы: {self.finished_courses}')
         return res
  
      
@@ -74,14 +75,44 @@ class Reviewer(Mentor):
     def __str__(self):
         res = (f'Имя: {self.name}\nФамилия: {self.surname}')
         return res
+
+def aw_rate_students(student_list, course_name):
+  result = 0
+  if course_name in Student.courses_in_progress or Student.finished_courses:
+    for element in Student.grades.values():
+      result += element
+    return round(sum(result) / len(result), 1)
         
 
-student_1 = Student('Emma', 'Stone', 'Gender')
+student_1 = Student('Emma', 'Stone', 'Female')
+student_1.add_courses_in_progress('Python')
+student_1.add_courses_in_progress('Git')
+student_1.finished_courses += ['Введение в программирование']
+
+student_2 = Student('Jack', 'Nolan', 'Male')
+student_2.add_courses_in_progress('Python')
+student_2.add_courses_in_progress('Git')
+student_2.finished_courses += ['Введение в программирование']
+
+lecturer_1 = Lecturer('Nora', 'Trae')
+lecturer_1.courses_attached += ['Python']
+lecturer_1.courses_attached += ['Git']
 
 reviewer_1 = Reviewer('Some', 'Buddy')
-student_1.add_courses_in_progress('Python')
-reviewer_1.courses_attached = ['Python']
+reviewer_1.courses_attached += ['Python']
+reviewer_1.courses_attached += ['Python']
+
+reviewer_2 = Reviewer('Rita', 'Pale')
+reviewer_2.courses_attached += ['Python']
+reviewer_2.courses_attached += ['Python']
 
 reviewer_1.rate_hw(student_1, 'Python', 10)
+reviewer_1.rate_hw(student_2, 'Python', 9)
+reviewer_1.rate_hw(student_1, 'Git', 10)
+reviewer_1.rate_hw(student_1, 'Git', 9)
+student_1.rate_lecturer(lecturer_1, 'Git', 10)
 
-print(student_1)
+print(student_1 > student_2)
+
+print(student_1.grades)
+print(lecturer_1.lecturer_grades)
